37600ab421d48f30dd209ea720f43221fa4779f0
Nsfr750 <2395874+Nsfr750@users.noreply.github.com>
2025-05-14 19:27:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from app import SQLiteApp
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title('SQLite Database Browser')
+    root.geometry('800x600')  # Set initial window size
+    
+    app = SQLiteApp(root)
+    root.protocol('WM_DELETE_WINDOW', app.close)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
